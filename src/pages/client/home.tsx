a160f466ef92b378f8ecdcfba2674f71df0ce6e4
Phan Thị Ngọc Mai <inet.ngocmai.v6@gmail.com>
2026-05-06 01:54:09 +0700

    feat: build UI client

--- src/pages/client/home.tsx
+++ src/pages/client/home.tsx
@@ -1,8 +1,287 @@
+import { getBooksAPI, getCategoryAPI } from "@/services/api";
+import { FilterTwoTone, ReloadOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Checkbox,
+  Col,
+  Divider,
+  Form,
+  InputNumber,
+  Pagination,
+  Rate,
+  Row,
+  Spin,
+  Tabs,
+  type FormProps,
+} from "antd";
+import { useEffect, useState } from "react";
+import "styles/home.scss";
+
+type FieldType = {
+  fullName: string;
+  password: string;
+  email: string;
+  phone: string;
+};
 const HomePage = () => {
- return (
-  <div>
-   Home page
-  </div>
- )
-}
-export default HomePage
+  const [listCategory, setListCategory] = useState<
+    { label: string; value: string }[]
+  >([]);
+
+  const [listBook, setListBook] = useState<IBookTable[]>([]);
+  const [current, setCurrent] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(5);
+  const [total, setTotal] = useState<number>(0);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [filter, setFilter] = useState<string>("");
+  const [sortQuery, setSortQuery] = useState<string>("sort=-sold");
+
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    const initCategory = async () => {
+      const res = await getCategoryAPI();
+      if (res && res.data) {
+        const d = res.data.map((item) => {
+          return { label: item, value: item };
+        });
+        setListCategory(d);
+      }
+    };
+    initCategory();
+  }, []);
+
+  useEffect(() => {
+    fetchBook();
+  }, [current, pageSize, filter, sortQuery]);
+
+  const fetchBook = async () => {
+    setIsLoading(true);
+    let query = `current=${current}&pageSize=${pageSize}`;
+    if (filter) {
+      query += `&${filter}`;
+    }
+    if (sortQuery) {
+      query += `&${sortQuery}`;
+    }
+
+    const res = await getBooksAPI(query);
+    if (res && res.data) {
+      setListBook(res.data.result);
+      setTotal(res.data.meta.total);
+    }
+    setIsLoading(false);
+  };
+
+  const handleOnChangePage = (pagination: {
+    current: number;
+    pageSize: number;
+  }) => {
+    if (pagination && pagination.current !== current) {
+      setCurrent(pagination.current);
+    }
+    if (pagination && pagination.pageSize !== pageSize) {
+      setPageSize(pagination.pageSize);
+      setCurrent(1);
+    }
+  };
+
+  const handleChangeFilter = (changedValues: any, values: any) => {
+    console.log(">>> check handleChangeFilter", changedValues, values);
+  };
+
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {};
+
+  const onChange = (key: string) => {
+    console.log(key);
+  };
+
+  const items = [
+    {
+      key: "1",
+      label: "Phổ biến",
+      children: <></>,
+    },
+    {
+      key: "2",
+      label: "Hàng mới",
+      children: <></>,
+    },
+    {
+      key: "3",
+      label: "Giá Thấp Đến Cao",
+      children: <></>,
+    },
+    {
+      key: "4",
+      label: "Giá Cao Đến Thấp",
+      children: <></>,
+    },
+  ];
+
+  return (
+    <div
+      className="homepage-container"
+      style={{ maxWidth: 1440, margin: "0 auto" }}
+    >
+      <Row gutter={[20, 20]}>
+        <Col md={4} xs={0} style={{ border: "1px solid green" }}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <span>
+              {" "}
+              <FilterTwoTone /> Bo loc tim kiem{" "}
+            </span>
+            <ReloadOutlined title="Reset" onClick={() => form.resetFields()} />
+          </div>
+          <Form
+            onFinish={onFinish}
+            form={form}
+            onValuesChange={(changedValues, values) =>
+              handleChangeFilter(changedValues, values)
+            }
+          >
+            <Form.Item
+              name="category"
+              label="Danh mục sản phẩm"
+              labelCol={{ span: 24 }}
+            >
+              <Checkbox.Group>
+                <Row>
+                  {listCategory?.map((item, index) => {
+                    return (
+                      <Col span={24} key={`index-${index}`}>
+                        <Checkbox value={item.value}>{item.label}</Checkbox>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Checkbox.Group>
+            </Form.Item>
+
+            <Divider />
+            <Form.Item label="Khoảng giá" labelCol={{ span: 24 }}>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Form.Item name={["range", "from"]}>
+                  <InputNumber
+                    name="from"
+                    min={0}
+                    placeholder="đ Từ"
+                    formatter={(value) =>
+                      `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                  />
+                </Form.Item>
+                <span>-</span>
+                <Form.Item name={["range", "to"]}>
+                  <InputNumber
+                    name="from"
+                    min={0}
+                    placeholder="đ Đến"
+                    formatter={(value) =>
+                      `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                  />
+                </Form.Item>
+              </div>
+              <div>
+                <Button
+                  onClick={() => form.submit()}
+                  style={{ width: "100%" }}
+                  type="primary"
+                >
+                  Áp dụng
+                </Button>
+              </div>
+            </Form.Item>
+
+            <Divider />
+            <Form.Item label="Đánh giá" labelCol={{ span: 24 }}>
+              <div>
+                <Rate value={5} disabled style={{ color: "#ffce3d" }} />
+                <span className="ant-rate-text"></span>
+              </div>
+              <div>
+                <Rate value={4} disabled style={{ color: "#ffce3d" }} />
+                <span className="ant-rate-text">trở lên</span>
+              </div>
+              <div>
+                <Rate value={3} disabled style={{ color: "#ffce3d" }} />
+                <span className="ant-rate-text">trở lên</span>
+              </div>
+              <div>
+                <Rate value={2} disabled style={{ color: "#ffce3d" }} />
+                <span className="ant-rate-text">trở lên</span>
+              </div>
+              <div>
+                <Rate value={1} disabled style={{ color: "#ffce3d" }} />
+                <span className="ant-rate-text">trở lên</span>
+              </div>
+            </Form.Item>
+          </Form>
+        </Col>
+
+        <Col md={20} xs={24}>
+          <Spin spinning={isLoading} tip="Loading...">
+            <div
+              style={{ padding: "20px", background: "#fff", borderRadius: 5 }}
+            >
+              <Row>
+                <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+              </Row>
+              <Row className="customize-row">
+                {listBook?.map((item, index) => {
+                  return (
+                    <div className="column" key={`book-${index}`}>
+                      <div className="wrapper">
+                        <div className="thumbnail">
+                          <img
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${item.thumbnail}`}
+                            alt="thumbnail"
+                          />
+                        </div>
+                        <div className="text" title={item.mainText}>
+                          {item.mainText}
+                        </div>
+                        <div className="price">
+                          {new Intl.NumberFormat("vi-VN", {
+                            style: "currency",
+                            currency: "VND",
+                          }).format(item?.price ?? 0)}
+                        </div>
+
+                        <div className="rating">
+                          <Rate
+                            value={5}
+                            disabled
+                            style={{ color: "#ffce3d", fontSize: 10 }}
+                          />
+                          <span>Đã bán {item?.sold ?? 0}</span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </Row>
+
+              <div style={{ marginTop: 30 }}></div>
+              <Divider />
+              <Row style={{ display: "flex", justifyContent: "center" }}>
+                <Pagination
+                  current={current}
+                  total={total}
+                  pageSize={pageSize}
+                  responsive
+                  onChange={(p, s) =>
+                    handleOnChangePage({ current: p, pageSize: s })
+                  }
+                />
+              </Row>
+            </div>
+          </Spin>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+export default HomePage;
